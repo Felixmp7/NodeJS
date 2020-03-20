c30d2b92a59747105f4cd955e2a296c24a4d41f2
Felix Pacheco <devfex.19@gmail.com>
2020-03-20 11:55:44 -0400

    adding console

---
+++ introduccion-02/console.js
@@ -0,0 +1,17 @@
+// console.log('Hola');
+
+const n1 = 10;
+const n2 = 14;
+// console.log("%d + %d = %d", n1, n2, (n1 + n2)); // Para mostrar valores numericos = %d
+
+const json = {
+  nombre: 'Felix',
+  apellido: 'Pacheco'
+}
+// console.log("%j", json); // Para mostrar objetos json = %j
+
+const name = 'Felix Manuel Pacheco Alonso';
+// console.log("%s", name ); // Para mostrar strings
+
+console.log(__filename); // Me devuelve el PATH hasta mi archivo
+console.log(__dirname); // Me devuelve el PATH hasta el directorio en el que se encuentra mi archivo
