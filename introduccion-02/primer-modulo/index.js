28ac3356dbdaf09119eadbb761a0fc64f107421c
Felix Pacheco <devfex.19@gmail.com>
2020-03-21 10:27:23 -0400

    adding files to pack

---
+++ introduccion-02/primer-modulo/index.js
@@ -0,0 +1,9 @@
+// JS común
+const conversion = require('./conversion')
+
+
+// ES 6
+// import conversion from './conversion';
+
+const pulgadas = conversion.pulgadasMetros(13);
+console.log(`13 pulgadas son ${pulgadas} metros`)
